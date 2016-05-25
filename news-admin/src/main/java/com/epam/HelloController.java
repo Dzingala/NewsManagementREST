7cb2641fc5888622f389c8cadb3ad7d67a59a98a
Dzingala <vanya32671@mail.ru>
2016-05-25 16:38:52 +0300

    [+]AuthorController

--- news-admin/src/main/java/com/epam/HelloController.java
+++ news-admin/src/main/java/com/epam/HelloController.java
@@ -1,13 +1,14 @@
 package com.epam;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloController {
+
+
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String init() {
         return "index";
@@ -27,7 +28,6 @@ public class HelloController {
     @RequestMapping(value = "/helloWorld",method = RequestMethod.GET)
     public String printHello(ModelMap model){
         model.addAttribute("message","Hello, it works finally!");
-        System.out.println("TTTTT");
         return "helloTest";
     }
 
---
+++ news-admin/src/main/java/com/epam/controller/AuthorController.java
@@ -0,0 +1,25 @@
+package com.epam.controller;
+
+import by.epam.lab.task.entity.Author;
+import by.epam.lab.task.exceptions.dao.DAOException;
+import by.epam.lab.task.exceptions.service.ServiceException;
+import by.epam.lab.task.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping(value="/author")
+public class AuthorController {
+    @Autowired
+    AuthorService authorService;
+
+    @RequestMapping(value = "/authors",method= RequestMethod.GET)
+    public String readAll(ModelMap modelMap) throws ServiceException, DAOException {
+        ArrayList<Author> authors=authorService.readAll();
+    }
+}
