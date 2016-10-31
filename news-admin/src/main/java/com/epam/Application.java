d496157f2763c8b16aca42c8d13fefbae50ec7d6
Dzingala <vanya32671@mail.ru>
2016-10-31 20:00:48 +0300

    [~]Spring boot application

--- news-admin/src/main/java/com/epam/Application.java
+++ news-admin/src/main/java/com/epam/Application.java
@@ -3,16 +3,17 @@ package com.epam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ImportResource;
 
 /**
- * Created by User on 10/29/2016.
+ * @author Ivan Dzinhala
  */
 @SpringBootApplication
 @ImportResource("classpath*:SpringModule.xml")
-@EnableAutoConfiguration
-@EntityScan(basePackages = "by.epam.lab.task.entity")
+@EnableAutoConfiguration(exclude = {HibernateJpaAutoConfiguration.class})
+//@EntityScan(basePackages = "by.epam.lab.task.entity")
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class);
