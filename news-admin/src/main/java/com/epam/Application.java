60a9e35ba27dbec2ac934f8aa6b27e55e76fc886
Ivan Dzinhala <Ivan_Dzinhala@epam.com>
2016-11-03 15:22:08 +0300

    [~]Slf4j, [~]Poms, etc.

--- news-admin/src/main/java/com/epam/Application.java
+++ news-admin/src/main/java/com/epam/Application.java
@@ -1,21 +1,21 @@
 package com.epam;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.annotation.ImportResource;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+//import org.springframework.boot.orm.jpa.EntityScan;
+//import org.springframework.context.annotation.ImportResource;
 
 /**
  * @author Ivan Dzinhala
  */
-@SpringBootApplication
-@ImportResource("classpath*:SpringModule.xml")
-@EnableAutoConfiguration(exclude = {HibernateJpaAutoConfiguration.class})
+//@SpringBootApplication
+//@ImportResource("classpath*:SpringModule.xml")
+//@EnableAutoConfiguration(exclude = {HibernateJpaAutoConfiguration.class})
 //@EntityScan(basePackages = "by.epam.lab.task.entity")
-public class Application {
-    public static void main(String[] args){
-        SpringApplication.run(Application.class);
-    }
-}
+//public class Application {
+//    public static void main(String[] args){
+//        SpringApplication.run(Application.class);
+//    }
+//}
