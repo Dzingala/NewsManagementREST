f7ff381d7cb56f686e12942844eb154e1712294b
Ivan Dzinhala <Ivan_Dzinhala@epam.com>
2016-11-11 12:51:25 +0300

    [~]Information is now transferred in the request body

--- news-admin/src/main/java/com/epam/controller/CommentController.java
+++ news-admin/src/main/java/com/epam/controller/CommentController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 
@@ -38,7 +35,7 @@ public class CommentController {
         return "redirect:/news/view/" + newsId;
     }
     @RequestMapping(value = "/comment/delete", method = RequestMethod.DELETE)
-    public void delete(@ModelAttribute Comment comment, @RequestParam Long newsId) throws ServiceException {
+    public void delete(@RequestBody Comment comment, @RequestParam Long newsId) throws ServiceException {
         commentService.delete(comment);
     }
 }
